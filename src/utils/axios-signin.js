6bd2ebc8bc89bcb506b9dee096fbfb9cd6bebd57
Kate McFaul <katemcfaul@gmail.com>
2018-11-17 10:51:54 -0800

    set up for sign in

--- src/utils/axios-signin.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://teen-center-sign-in.firebaseio.com/'
-});
-
-export default instance;
---
+++ src/utils/secrets.js
@@ -0,0 +1,5 @@
+const secrets = {
+  BASE_URL: "https://teen-center-sign-in.firebaseio.com/"
+}
+
+export default secrets;
